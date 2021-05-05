208af5cc08e29a9c29b2034a7815e4f7cdfb016f
Giorgio Vecchi <gvecchi@hotmail.com>
2021-05-05 15:45:44 +0200

    Add project files.

---
+++ Persone/Persona.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persone
+{
+    class Persona
+    {
+        public string Nome { get; }
+        public string Cognome { get; }
+        public DateTime DataDiNascita { get; }
+        public string CodiceFiscale { get; }
+
+        public Persona(string nome, string cognome, DateTime dataDiNascita, string codiceFiscale)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataDiNascita = dataDiNascita;
+            CodiceFiscale = codiceFiscale;
+        }
+
+        public int Eta { get 
+            {
+                //TimeSpan diff = DateTime.Today.Subtract(DataDiNascita);
+                //return (int)(diff.Days / 365.25);
+
+                // datadinascita 6/5/2000   20000505
+                // 05/05/2021               20210505
+
+                int anni = DateTime.Today.Year - DataDiNascita.Year;
+
+                //if (DataDiNascita.Month < DateTime.Today.Month ||
+                //    (DataDiNascita.Month == DateTime.Today.Month &&
+                //    DataDiNascita.Day <= DateTime.Today.Day))
+                //    return anni;
+                //return anni - 1;
+
+                //return ((DateTime.Today.Year * 10000 + 
+                //    DateTime.Today.Month * 100 + 
+                //    DateTime.Today.Day) -
+                //    (DataDiNascita.Year * 10000 + 
+                //    DataDiNascita.Month * 100 + 
+                //    DataDiNascita.Day)) / 10000;
+
+                DateTime dataCompleanno = new DateTime(DateTime.Today.Year,
+                    DataDiNascita.Month, 1).AddDays(DataDiNascita.Day - 1);
+
+                if (DateTime.Today < dataCompleanno)
+                    return anni - 1;
+                return anni;
+            } 
+        }
+
+        public string DatiTSV  // CSV Comma Separated Values, TSV Tab Separated Values
+        {
+            get
+            { return Nome + '\t' + Cognome + '\t' + DataDiNascita.ToShortDateString() + '\t' + CodiceFiscale; }
+        }
+
+        public string Dati
+        {
+            get
+            { return Nome + ' ' + Cognome + ' ' + DataDiNascita.ToShortDateString() + ' ' + CodiceFiscale + ' ' + Eta; }
+        }
+    }
+}
---
+++ Persone/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Persone
+{
+    class Program
+    {
+        const string fileName = @"persone.txt";
+
+        static void Salva(Dictionary<string, Persona> elenco)
+        {
+            using (StreamWriter sw = new StreamWriter(fileName))
+            {
+                foreach (Persona p in elenco.Values)
+                    sw.WriteLine(p.DatiTSV);
+            }
+        }
+
+        static Dictionary<string, Persona> Carica()
+        {
+            Dictionary<string, Persona> elenco = new Dictionary<string, Persona>();
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string riga = sr.ReadLine();
+                    string[] pezzi = riga.Split('\t');    // spezza la stringa in corrispondenza di tab
+                    Persona p = new Persona(pezzi[0], pezzi[1], DateTime.Parse(pezzi[2]), pezzi[3]);
+                    elenco.Add(p.CodiceFiscale, p);
+                }
+            }
+
+            return elenco;
+        }
+
+        static void Main(string[] args)
+        {
+            // Dictionary<string, Persona> elenco = new Dictionary<string, Persona>();
+
+            Dictionary<string, Persona> elenco = Carica();
+
+            foreach (Persona p in elenco.Values)
+                Console.WriteLine(p.Dati);
+
+            //Persona p1 = new Persona("Pinco", "Pallino", new DateTime(1995, 4, 3), "ABCDEF");
+            //Persona p2 = new Persona("Gino", "Pino", new DateTime(1995, 4, 3), "GPGPGP");
+            //Persona p3 = new Persona("Luisa Maria", "Pia", new DateTime(1995, 4, 3), "MARPIA");
+            //elenco.Add(p1.CodiceFiscale, p1);
+            //elenco.Add(p2.CodiceFiscale, p2);
+            //elenco.Add(p3.CodiceFiscale, p3);
+
+            //List<Persona> lista = new List<Persona>();
+
+
+            //Salva(elenco);
+
+            //lista.Add(p1);
+            //lista.Add(p2);
+            //lista.Add(p3);
+
+            Console.Write("Ricerca per codice fiscale: ");
+            string cf = Console.ReadLine();
+
+            if (elenco.ContainsKey(cf))
+                Console.WriteLine(elenco[cf].Dati);
+            else
+                Console.WriteLine("Codice fiscale inesistente");
+
+            //bool presente = false;
+            //foreach (Persona p in lista)
+            //    if (p.CodiceFiscale == cf)
+            //    {
+            //        Console.WriteLine(p.Dati);
+            //        presente = true;
+            //        break;
+            //    }
+
+            //if (!presente)
+            //    Console.WriteLine("Codice fiscale inesistente");
+        }
+    }
+}
